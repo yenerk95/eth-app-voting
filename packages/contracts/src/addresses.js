527cb7e80a93c74e2f83eaba1731d1d3764ae0e1
yenerk95 <yenerkaraca@gmail.com>
2020-11-26 10:19:12 +0300

    changes

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,5 +1,6 @@
 const addresses = {
-  ipfs: "0xf1bc720a690280c01d5dcd3de09740279edd72e0",
+  ipfs: "0xdc9802a1e6d201acf1c37b66c8e7cea28f11dbde",
+  storage: "0xAe253B54ddC65A30A1A14710469F7DFf063557b5",
 };
 
 export default addresses;
--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -7,7 +7,6 @@ import logo from "./ethereumLogo.png";
 import { addresses, abis } from "@project/contracts";
 
 import "./App.css";
-
 const ZERO_ADDRESS =
   "0x0000000000000000000000000000000000000000000000000000000000000000";
 
@@ -19,6 +18,12 @@ const ipfsContract = new ethers.Contract(
   abis.ipfs,
   defaultProvider
 );
+const storageContract = new ethers.Contract(
+  addresses.storage,
+  abis.storage,
+  defaultProvider
+);
+
 
 async function initIpfs() {
   node = await IPFS.create();
@@ -29,10 +34,26 @@ async function initIpfs() {
 async function readCurrentUserFile() {
   const result = await ipfsContract.userFiles(
     defaultProvider.getSigner().getAddress()
+    
+  
   );
   console.log({ result });
+ 
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];//metamask current account
 
+  
   return result;
+}
+async function getaccount(){
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  Object.values(accounts);
+  const account =JSON.stringify(accounts[0]);
+  console.log("FUNC");
+
+  console.log({ account });
+
+return account
 }
 
 function App() {
@@ -51,13 +72,78 @@ function App() {
     readFile();
   }, []);
 
+
+
+
+  const [accountHash, setAccountHash] = useState("");
+  useEffect(() => {
+    window.ethereum.enable();
+  }, []);
+
+  useEffect(() => {
+    async function readFile() {
+      const file1 = await getaccount();
+
+      if (file1 !== ZERO_ADDRESS) setAccountHash(file1);
+    }
+    readFile();
+  }, []);
+
+
+
+
   async function setFile(hash) {
     const ipfsWithSigner = ipfsContract.connect(defaultProvider.getSigner());
     const tx = await ipfsWithSigner.setFile(hash);
     console.log({ tx });
 
     setIpfsHash(hash);
+    
+    ipfsWithSigner.store(hash);
+
+  
   }
+
+
+  async function Buyer() {
+
+    
+    const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
+  
+    await storageWithSigner.buyerUpload(ipfsHash);
+  
+  }
+
+
+  async function Seller() {
+
+    
+    const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
+    
+    await storageWithSigner.sellerUpload(ipfsHash);
+
+  
+  }
+  async function Set() {
+
+    
+    const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
+    await storageWithSigner.SetEndTime(itemInput);
+
+  
+  }
+  async function Extend() {
+
+    
+    const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
+    
+    await storageWithSigner.ExtendTime(itemInput1);
+
+  
+  }
+  
+  console.log(ipfsHash);
+  
 
   const uploadFile = useCallback(async (file) => {
     const files = [
@@ -83,6 +169,14 @@ function App() {
     onDrop,
   });
 
+  const [itemInput, setItemInput] = useState("");
+  console.log(itemInput);
+  const [itemInput1, setItemInput1] = useState("");
+  console.log(itemInput1);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -98,22 +192,62 @@ function App() {
             </p>
           )}
         </div>
+        
+        <div>
+          {ipfsHash !== "" ? (   
+            <p>IPFS Hash: <mark className="mark-red">"{ipfsHash}"</mark></p>
+            ) : (
+              ""
+            )}
+        
+        </div>  
+        <div>User Hash:<mark className="mark-yellow">{accountHash}</mark></div>
+   
         <div>
           {ipfsHash !== "" ? (
+            
+
             <a
+            
               href={`https://ipfs.io/ipfs/${ipfsHash}`}
               target="_blank"
               rel="noopener noreferrer"
             >
-              See current user file
+              See current user file 
             </a>
           ) : (
             "No user file set yet"
           )}
+
         </div>
+        <div>Buyer Can Set and Extend Time</div>
+      
+        <div>
+          Set End Time:
+
+         
+          <input type="text" placeholder="Enter an item" value={itemInput} onChange={e => setItemInput(e.target.value)}
+        
+          />
+          <button onClick={Set}>Buyer Set</button>
+         </div>
+         <div>
+          Extend Time:
+          <input type="text" placeholder="Enter an item" value={itemInput1} onChange={e => setItemInput1(e.target.value)}
+         
+          />
+          <button onClick={Extend}>Buyer Extend</button>
+         </div>
+         <div>Buyer Clicks to Upload to Smart Contract:   <button onClick={Buyer}>Buyer Upload</button></div>
+         <div>Seller Clicks to Upload to Smart Contract:   <button onClick={Seller}>Seller Upload</button></div>
+         <script></script>
       </header>
+
     </div>
   );
+  
 }
 
 export default App;
+
+
