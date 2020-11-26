1c6c0b88138bff6c797e8756cfb9d0c7bb34026e
yenerk95 <yenerkaraca@gmail.com>
2020-11-26 13:14:17 +0300

    build

---
+++ packages/react-app/build/precache-manifest.31387cf883f0344595229b8b5e0c7c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699cd123fd1b817f17442045f8316375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b54dc7d1cdf169288f2",
+    "url": "/static/css/main.f96187e8.chunk.css"
+  },
+  {
+    "revision": "371d6aa8e3110d62e522",
+    "url": "/static/js/2.91d23e11.chunk.js"
+  },
+  {
+    "revision": "4bd9f6e66e05ad73f9cd35eeb7c10c25",
+    "url": "/static/js/2.91d23e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b54dc7d1cdf169288f2",
+    "url": "/static/js/main.e4c89003.chunk.js"
+  },
+  {
+    "revision": "8f96027f25cd7b7e59d5",
+    "url": "/static/js/runtime-main.db7e2936.js"
+  },
+  {
+    "revision": "0020ce378309fd7c0047d0fde259a770",
+    "url": "/static/media/ethereumLogo.0020ce37.png"
+  }
+]);
