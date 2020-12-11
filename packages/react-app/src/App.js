4d65be52403fb860f100a6b3226c86356ebcc59d
yenerk95 <yenerkaraca@gmail.com>
2020-12-12 00:46:52 +0300

    voting app update

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useDropzone } from "react-dropzone";
+
 import { ethers } from "ethers";
-import IPFS from "ipfs";
 
 import logo from "./ethereumLogo.png";
 import { addresses, abis } from "@project/contracts";
@@ -11,38 +10,10 @@ import "./App.css";
 const ZERO_ADDRESS =
   "0x0000000000000000000000000000000000000000000000000000000000000000";
 
-let node;
-
-var dict = {};
 
 const defaultProvider = new ethers.providers.Web3Provider(window.ethereum);
-//const ipfsContract = new ethers.Contract(
- // addresses.ipfs,
-  //abis.ipfs,
-  //defaultProvider
-//);
 var storageContract;
-console.log( addresses.ipfs);
 
-async function initIpfs() {
-  node = await IPFS.create();
-  const version = await node.version();
-  console.log("IPFS Node Version:", version.version);
-}
-
-async function readCurrentUserFile() {
- // const result = await ipfsContrstact.userFiles(
-  //  defaultProvider.getSigner().getAddress()
-    
-  
-  //);
- // console.log({ result });
- 
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];//metamask current account
-  
-  return dict[account];
-}
 async function getaccount(){
   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
   Object.values(accounts);
@@ -56,21 +27,7 @@ return account
 
 
 function App() {
-  const [ipfsHash, setIpfsHash] = useState("");
-  useEffect(() => {
-    initIpfs();
-    window.ethereum.enable();
-  }, []);
-
-  useEffect(() => {
-    async function readFile() {
-      const file = await readCurrentUserFile();
-
-      if (file !==undefined) setIpfsHash(file);
-    }
-    readFile();
-  }, []);
-
+  
 
   const [accountHash, setAccountHash] = useState("");
   useEffect(() => {
@@ -87,21 +44,6 @@ function App() {
     readFile();
   }, []);
 
-
-
-  async function setFile(hash) {
-    dict[accountHash]=hash;
-
-   // const ipfsWithSigner = ipfsContract.connect(defaultProvider.getSigner());
-   //const tx = await ipfsWithSigner.setFile(hash);
-   // console.log({ tx });
-
-    setIpfsHash(hash);
-    
-    //yipfsWithSigner.store(hash);
-
-  
-  }
 
    function SetContract() {
     storageContract = new ethers.Contract(itemInput2, abis.storage, defaultProvider);
@@ -132,7 +74,7 @@ function App() {
   
     try {
       const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.buyerUpload(ipfsHash,itemInput);
+      await storageWithSigner.buyerUpload(1,itemInput);
       
     }
     catch(err) {
@@ -143,15 +85,15 @@ function App() {
     
   
   }
+ 
 
 
-  async function Seller() {
+  async function voteright() {
 
-
-    
+  
     try {
       const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.sellerUpload(ipfsHash);
+      await storageWithSigner. giveRightToVote(itemInput);
       
     }
     catch(err) {
@@ -163,13 +105,30 @@ function App() {
   
   }
 
-  async function Extend() {
+  async function removevoteright() {
+
+  
+    try {
+      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
+      await storageWithSigner. removeRightToVote(itemInput1);
+      
+    }
+    catch(err) {
+      console.log(err.message);
+      document.getElementById("text-box").className = "color-red";
+      document.getElementById("text-box").innerHTML = err.message
+    }
+    
+  
+  }
+
+  async function endtime() {
 
 
 
     try {
       const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.ExtendTime(itemInput1);
+      await storageWithSigner.ExtendTime(itemInput3);
       
     }
     catch(err) {
@@ -181,10 +140,28 @@ function App() {
   
   }
 
+  async function vote() {
+
+
+
+    try {
+      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
+      await storageWithSigner.vote(itemInput4);
+      
+    }
+    catch(err) {
+      console.log(err.message);
+      document.getElementById("text-box").className = "color-red";
+      document.getElementById("text-box").innerHTML = err.message
+    }
+    
+  
+  }
+  
 
   async function status() {
 
-    var status_array=["ON", "BUYER_UPLOADED", "SELLER_UPLOADED", "DOC_OK", "DOC_DEFECT", "DOC_REJECTED"];
+    var status_array=["ON", "BUYER_UPLOADED", "SELLER_UPLOADED", "DOC_OK", "DOC_DEFECT", "DOC_REJECTED","MONEY_SENT"];
     var time_array=["ON_TIME", "OUT_OF_TIME"];
 
 
@@ -213,177 +190,11 @@ function App() {
 
 
 
-  async function compliancetrue() {
-
-
-
-    try {
-      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.checkCompliance();
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }
-    
   
-  }
 
-
-  async function waivetrue() {
-
-
-
-    try {
-      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.waiveDiscrepancies(true);
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }
-    
-  
-  }
-
-  async function waivefalse() {
-
-    try {
-      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.waiveDiscrepancies(false);
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }   
-  
-  }
-  async function destroy() {
-
-
-
-    try {
-      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.destroycontract(false);
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }
-    
-  
-  }
-
-  async function moneyupload() {
-
-
-
-    try {
-
-
-      var transactionParameters = await{
-
-        to: itemInput2.toString(), // Required except during contract publications.
-        from: window.ethereum.selectedAddress, // must match user's active address.
-        value: '0x'+(itemInput4*1000000000000000000).toString(16)
-
-
-      };
-      
-      // txHash is a hex string
-      // As with any RPC call, it may throw an error
-      const txHash = await window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [transactionParameters],
-      });
-      // const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      // await storageWithSigner.Ether_Upload();
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }
-    
-  
-  }
-  async function withdraw() {
-
-
-
-    try {
-      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-      await storageWithSigner.withdrawFunds();
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }
-    
-  
-  }
 
   
 
-  async function getContractFile() {
-
-    
-    try {
-      const storageWithSigner = storageContract.connect(defaultProvider.getSigner());
-
-      var contractfile = await storageWithSigner.See_Doc_Hash(itemInput3);
-      document.getElementById("myAnchor").innerHTML ="See Contract File";
-      document.getElementById("myAnchor").href = `https://ipfs.io/ipfs/${contractfile}`;
-      document.getElementById("myAnchor").target = "_blank";
-      
-    }
-    catch(err) {
-      console.log(err.message);
-      document.getElementById("text-box").className = "color-red";
-      document.getElementById("text-box").innerHTML = err.message
-    }
-
-       
-  
-  }
-  
-  console.log(ipfsHash);
-  
-
-  const uploadFile = useCallback(async (file) => {
-    const files = [
-      {
-        path: file.name + file.path,
-        content: file,
-      },
-    ];
-
-    for await (const result of node.add(files)) {
-      await setFile(result.cid.string);
-    }
-  }, []);
-
-  const onDrop = useCallback(
-    (acceptedFiles) => {
-      uploadFile(acceptedFiles[0]);
-    },
-    [uploadFile]
-  );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    multiple: false,
-    onDrop,
-  });
 
   const [itemInput, setItemInput] = useState("");
   console.log(itemInput);
@@ -395,7 +206,8 @@ function App() {
   console.log(itemInput3);
   const [itemInput4, setItemInput4] = useState("");
   console.log(itemInput4);
-
+ 
+ 
   
 
 
@@ -415,72 +227,27 @@ function App() {
       <div id="text-box1"></div>
       </body>
 
-
-        <div {...getRootProps()} style={{"width" : "60%", "height" : "50%", "border" : "5px solid #191b1f", "margin" : "5px","padding" : "10px",cursor: "pointer", "background-color" :"rgb(25, 27, 31, 0.3)"}}>
-          <img src={logo} className="App-logo" alt="react-logo" />
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <div>Drop the files here ...</div>
-          ) : (
-            <div>
-              Drag 'n' drop some files here to upload to IPFS (or click the
-              logo)
-            </div>
-          )}
-
-        </div>
-        <body className="files">
-
-        {ipfsHash !== "" ? (   
-            <div>Current IPFS Hash: <mark className="mark-red">"{ipfsHash}"</mark></div>
-            ) : (
-              ""
-            )}
+        <body className="fintech">
         <div>
-          {ipfsHash !== "" ? (
-            <a
-              href={`https://ipfs.io/ipfs/${ipfsHash}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              See current user file 
-            </a>
-          ) : (
-            "No user file set yet"
-          )}
-
-        </div>
-        </body>
-        <body className="buyer">
-        <div>
-         Buyer Sets Time and Uploads to The Contract: <input type="text" placeholder="Enter Time" value={itemInput} onChange={e => setItemInput(e.target.value)}/>
-         <button onClick={Buyer}>Buyer Upload</button>
-        </div>
-        <div>Buyer Uploads Money to The Contract: <input type="text" placeholder="Enter ETH Amount" value={itemInput4} onChange={e => setItemInput4(e.target.value)}/><button onClick={moneyupload}>Buyer Upload Money </button></div>
-        <div>
-        Buyer Extends Time:
-        <input type="text" placeholder="Enter Time" value={itemInput1} onChange={e => setItemInput1(e.target.value)}/>
-        <button onClick={Extend}>Buyer Extend</button>
+          Fintech Gives Right to Vote: <input type="text" placeholder="Hash" value={itemInput} onChange={e => setItemInput(e.target.value)}/>
+        <button onClick={voteright}>Give</button>
         </div>
         <div>
-           Buyer Waives or Terminates:  <button onClick={waivetrue}>Waive Discrepancies</button>  <button onClick={waivefalse}>Terminate The Contract</button> 
-         </div>
+          Fintech Removes Right to Vote: <input type="text" placeholder="Hash" value={itemInput1} onChange={e => setItemInput1(e.target.value)}/>
+        <button onClick={removevoteright}>Remove</button>
+        </div>
+        <div>
+        Fintechs Sets Time:
+        <input type="text" placeholder="Enter Time" value={itemInput3} onChange={e => setItemInput3(e.target.value)}/>
+        <button onClick={endtime}>Set</button>
+        </div>
          </body>
-         <body className="seller">
-         <div>Seller Uploads to The Contract: <button onClick={Seller}>Seller Upload</button>
-         </div>
-         </body>
-         <body className="fintech">
-
-          <div>
-          Smart Contract Files: <input type="text" placeholder="Buyer or Seller Hash" value={itemInput3} onChange={e => setItemInput3(e.target.value)}/>
-          <button onClick={getContractFile}>See Contract Files</button>
-          </div>
-          <a id="myAnchor" href=""></a>
-          <div>
-          (Ballot is Needed) Fintech Check Compliance or Destroy Contract:  <button onClick={compliancetrue}>Check Compliance</button> <button onClick={destroy}>Destroy Contract</button>
-          </div>
-          <button onClick={withdraw}>Withdraw Funds</button>
+         <body className="banks">
+         <div>
+        Banks Vote:
+        <input type="text" placeholder="Proposal" value={itemInput4} onChange={e => setItemInput4(e.target.value)}/>
+        <button onClick={vote}>Vote</button>
+        </div>
 
          </body>
 
