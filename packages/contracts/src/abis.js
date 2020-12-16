b3403d0925f2a9e51e6ccda98dacb6384469f1be
yenerk95 <yenerkaraca@gmail.com>
2020-12-16 13:16:31 +0300

    .gitignore is now working

--- packages/contracts/src/abis.js
+++
@@ -1,9 +0,0 @@
-
-import ipfsStorage from "./abis/IpfsStorage.json";
-import Storage from "./abis/Storage.json";
-const abis = {
-  ipfs: ipfsStorage,
-  storage: Storage,
-};
-
-export default abis;
